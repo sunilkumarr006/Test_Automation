ea7bd9ab5467b90e1114e22db8691b040b960034
sunilkumarr006 <sunilkumarr006@gmail.com>
2025-06-08 22:52:05 +0530

    Update multiply.java

--- multiply.java
+++ multiply.java
@@ -1,1 +1,9 @@
-
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b =40;
+        int c = a*b;
+        System.out.println(c);
+    }
+    
+}
